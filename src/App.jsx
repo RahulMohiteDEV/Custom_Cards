f09621570dbef7678ede803efbe0df4ec62b9dd8
rahul <mohiterahul4670@gmail.com>
2025-09-29 11:49:11 +0530

    Custom Cardes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Backgound from './components/Backgound';
+import Forground from './components/Forground';
+
+const App = () => {
+  return (
+    <div className='relative w-full h-screen bg-gray-900'>
+  
+       <Backgound/>
+       <Forground/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Backgound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Backgound = () => {
+  return (
+  
+     <div className='fixed z-[2] w-full h-screen'>
+       <div className='absolute top-[5%] w-full py-10 text-center text-white font-semibold text-xl '>
+       Documents
+      </div>
+      <h1 className=' absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[13vw] leading-none tracking-tighter font-semibold text-white'>Docs.</h1>
+       </div>
+   
+  )
+}
+
+export default Backgound
